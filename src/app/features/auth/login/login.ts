b8c519dcd85c99a1b1eb6d8d97164cb8a4f58525
Alvarez <jamespatageules@hotmail.com>
2026-04-14 23:42:06 +0200

    chore

--- src/app/features/auth/login/login.ts
+++ src/app/features/auth/login/login.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {Card} from 'primeng/card';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [
+    Card
+  ],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
