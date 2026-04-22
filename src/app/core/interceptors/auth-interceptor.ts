4f7c70fe2e10f6d79235ea0d1feabc99f76367dd
AdrienAlva <58664882+AdrienAlva@users.noreply.github.com>
2026-04-23 00:05:19 +0200

    Auth http interceptor / Guard / Prime icons / Tailwind  (#3)
    * Suppression du proxy Angular et mise à jour de l'URL de l'API (#2)
    
    Co-authored-by: Alvarez <jamespatageules@hotmail.com>
    
    * Ajout de l'intercepteur pour le token d'auth
    
    * Ajout de l'intercepteur dans les providers de l'app + logs
    
    * component dashboard init
    
    * auth guard
    
    * Make `Dashboard` component standalone
    
    * Add route for `Dashboard` with `authGuard` protection
    
    * draw-close.regular.otf font update
    
    * primeicons import added
    
    * Add TailwindCSS integration with PostCSS plugin
    
    * Update login page layout with TailwindCSS and improve form structure
    
    ---------
    
    Co-authored-by: Alvarez <jamespatageules@hotmail.com>

--- src/app/core/interceptors/auth-interceptor.ts
+++ src/app/core/interceptors/auth-interceptor.ts
@@ -1,5 +1,40 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import {AuthService} from '@core/services/auth-service';
 
+
+/**
+ * Intercepts HTTP requests to attach an authorization token if available.
+ *
+ * This interceptor checks for a token from the authentication service
+ * and, if present, clones the original request to include the token in
+ * the `Authorization` header. If no token is available, the original
+ * request is passed through without modification.
+ *
+ * @param {HttpRequest} req - The outgoing HTTP request.
+ * @param {HttpHandler} next - The next handler in the chain to forward the request.
+ * @returns {Observable<HttpEvent<any>>} An observable that emits the HTTP events.
+ */
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
-  return next(req);
+  const authService = inject(AuthService);
+  const token = authService.getToken();
+
+  console.log('Interceptor called for:', req.url);
+
+  if (!token) {
+    return next(req);
+  }
+
+  const authReq = req.clone({
+    setHeaders: {
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  console.log('Interceptor called');
+  console.log('URL:', req.url);
+  console.log('Token present:', !!token);
+  console.log('Authorization header:', authReq.headers.get('Authorization'));
+
+  return next(authReq);
 };
