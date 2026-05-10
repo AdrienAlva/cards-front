26e31855cb70b04da00759cbf08a1d8c232bc44b
Alvarez <jamespatageules@hotmail.com>
2026-05-10 22:49:48 +0200

    prototype games selection cards

--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import {Navbar} from '@shared/components/navbar/navbar';
+import {Button} from 'primeng/button';
+import {Card} from 'primeng/card';
 
 @Component({
   selector: 'app-dashboard',
   imports: [
-    Navbar
+    Navbar,
+    Button,
+    Card
   ],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
