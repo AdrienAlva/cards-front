4ec7350fa825df37881a6fc7efae9ea4ea6fcb64
Alvarez <jamespatageules@hotmail.com>
2026-05-13 23:52:46 +0200

    feat(user-profile, dashboard): refactor layout with reusable container class and add routing for new game creation

--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -1,7 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {Navbar} from '@shared/components/navbar/navbar';
 import {Button} from 'primeng/button';
 import {Card} from 'primeng/card';
+import {Router} from '@angular/router';
+import {ROUTES_ENUM} from '@shared/enums/routes.enum';
 
 @Component({
   selector: 'app-dashboard',
@@ -14,4 +16,12 @@ import {Card} from 'primeng/card';
   styleUrl: './dashboard.css',
   standalone: true,
 })
-export class Dashboard {}
+export class Dashboard {
+
+  private readonly router = inject(Router);
+
+  toNewGame(): void {
+    this.router.navigate([ROUTES_ENUM.game()]);
+  }
+
+}
